28367aeeae874c7cee92da66c2bb31901d1b9c13
jpmartinez <jpmc21891@gmail.com>
2020-03-23 23:53:46 -0300

    Updates

---
+++ precache-manifest.c4137284c7217c0363a6c78a8571e5bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a38222c38331a4aa40e5d09bc01ae17",
+    "url": "/dicey-addiction/index.html"
+  },
+  {
+    "revision": "5c34fb2398751f99d91b",
+    "url": "/dicey-addiction/static/css/main.97d761b6.chunk.css"
+  },
+  {
+    "revision": "3e4cd4fbc9807fd9793e",
+    "url": "/dicey-addiction/static/js/2.11f68007.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/dicey-addiction/static/js/2.11f68007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c34fb2398751f99d91b",
+    "url": "/dicey-addiction/static/js/main.de35b591.chunk.js"
+  },
+  {
+    "revision": "6d7e76acd3927ad317ed",
+    "url": "/dicey-addiction/static/js/runtime-main.c738e06a.js"
+  }
+]);
